e7a9f351a938c813e4ed8a4c6700864962b1084f
Gabriel Perez <perezsanchezgabirel@gmail.com>
2017-12-11 22:15:17 -0200

    Arreglando el nombre al enviar el formulario

--- public_html/scripts/site.js
+++ public_html/scripts/site.js
@@ -50,7 +50,7 @@ $(function(){
         $.post('http://cerramientostechos.com.ar/mailer.php', {
             accion: 'consulta-rapida',
             pagina_visitada: $("#nombre").attr("nombrePagina"),
-            nombre: $("#nombre").attr("nombre"),
+            nombre: $("#nombre").attr("value"),
             telefono: $("#telefono").attr("value"),
             contacto: $("#contacto").attr("value"),
             consulta: $("#consulta").attr("value"),
